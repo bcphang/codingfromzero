96b6e85052030db507562deac7505c4d295b7460
bcphang <fate_king@yahoo.com>
2016-02-16 00:11:52 +0800

    Update contact_me.js

--- js/contact_me.js
+++ js/contact_me.js
@@ -18,13 +18,13 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+                //url: "././mail/contact_me.php",
+                url: "//formspree.io/bc.phang@gmail.com",
+                dataType: "json"
                 type: "POST",
                 data: {
                     name: name,
-                    phone: phone,
-                    email: email,
-                    message: message
+                    email: email
                 },
                 cache: false,
                 success: function() {
